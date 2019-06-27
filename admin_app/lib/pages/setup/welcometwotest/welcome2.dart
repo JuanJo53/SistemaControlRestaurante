2ab31db54a9ea5ed4e18a24dd958373c94b6aa3e
Carlos Peralta <peralta.lima.carlos@gmail.com>
2019-06-26 21:54:11 -0400

    Borrando Redes Sociales

--- admin_app/lib/pages/setup/welcometwotest/welcome2.dart
+++ admin_app/lib/pages/setup/welcometwotest/welcome2.dart
@@ -163,44 +163,6 @@ class _SecondWelcomeState extends State<SecondWelcome> {
                   SizedBox(
                     height: ScreenUtil.getInstance().setHeight(40),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SocialIcon(
-                        colors: [
-                          Color(0xFF102397),
-                          Color(0xFF187adf),
-                          Color(0xFF00eaf8),
-                        ],
-                        iconData: CustomIcons.facebook,
-                        onPressed: () {},
-                      ),
-                      SocialIcon(
-                        colors: [
-                          Color(0xFFff4f38),
-                          Color(0xFFff355d),
-                        ],
-                        iconData: CustomIcons.googlePlus,
-                        onPressed: () {},
-                      ),
-                      SocialIcon(
-                        colors: [
-                          Color(0xFF17ead9),
-                          Color(0xFF6078ea),
-                        ],
-                        iconData: CustomIcons.twitter,
-                        onPressed: () {},
-                      ),
-                      SocialIcon(
-                        colors: [
-                          Color(0xFF00c6fb),
-                          Color(0xFF005bea),
-                        ],
-                        iconData: CustomIcons.linkedin,
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
                   SizedBox(
                     height: ScreenUtil.getInstance().setHeight(30),
                   ),
